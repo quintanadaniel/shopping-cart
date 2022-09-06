c1ba9325a64f7cc76e1711b91f882e55ffb3b0f8
Daniel Quintana <quintana.danielarmando@gmail.com>
2022-09-06 16:51:52 +0100

    added method to save and get products by id

--- application/models/data_base.py
+++ application/models/data_base.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from dataclasses import field
 from typing import Dict
 from typing import Optional
 
@@ -8,5 +9,14 @@ from application.models.product import Product
 
 @dataclass()
 class DataBase:
-    product: Optional[Dict[int, Product]]
+    product: Optional[Dict[str, Product]]
     car: Optional[Dict[int, Car]]
+
+    def save_product(self, product: Product):
+        self.product[product.code] = Product(
+            code=product.code, name=product.name, price=product.price
+        )
+
+    def get_product_by_code(self, code_id: str) -> Product:
+        product = self.product[code_id]
+        return product
