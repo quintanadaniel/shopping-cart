b288f7752ead3a5055a2f80dfdfd224a0f99eb8b
Daniel Quintana <quintana.danielarmando@gmail.com>
2022-09-22 11:10:25 +0100

    modified car model structure adding an incremental id

--- application/models/car.py
+++ application/models/car.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from dataclasses import field
+from itertools import count
+
 from typing import Dict
 from typing import Optional
 
@@ -8,7 +11,7 @@ from application.models.product import Product
 
 @dataclass
 class Car:
-    id: int
+    id: int = field(init=False, default=count(1).__next__())
     product: Dict[int, Product]
     discount: Optional[Dict[int, Discount]]
     quantity: float
