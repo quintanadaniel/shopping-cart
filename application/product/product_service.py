438e36d0cf5aebc61383fb714ec51ebe7eb597fd
Daniel Quintana <quintana.danielarmando@gmail.com>
2022-09-22 11:11:44 +0100

    created  endpoint POST and GET data from product's model created your own test

--- application/product/product_service.py
+++ application/product/product_service.py
@@ -1,44 +1,57 @@
+from typing import Optional
+
 from flask import request
 
+from application.exceptions.product_is_invalid_exception import (
+    ProductIsInvalidException,
+)
+from application.exceptions.product_is_none_exception import ProductIsNoneException
 from application.models.data_base import DataBase
 from application.models.product import Product
 
 
 class ProductService:
-    def __init__(self, db: DataBase):
-        self.db = db
+    """Class object to include all the services about Products"""
 
-    def create(self, request: request) -> None:
+    def __init__(self, database: DataBase):
+        self.database = database
 
-        self.__validate_code(request["code"])
-        self.__validate_name(request["name"])
-        self.__validate_price(request["price"])
+    def create(self, req: request) -> None:
+        """Method to create new Products"""
+        self.__validate_code(req["code"])
+        self.__validate_name(req["name"])
+        self.__validate_price(req["price"])
 
-        product = Product(
-            code=request["code"], name=request["name"], price=float(request["price"])
-        )
+        product = Product(code=req["code"], name=req["name"], price=float(req["price"]))
 
-        self.db.save_product(product=product)
+        self.database.save_product(product=product)
 
-    def __validate_code(self, code: str) -> None:
-        if code is None:
-            raise  # TODO create exception
+    @staticmethod
+    def __validate_code(code: str) -> None:
+        if code is None or code == "":
+            raise ProductIsNoneException("code")
+        if not isinstance(code, str):
+            raise ProductIsInvalidException("code")
 
-    def __validate_name(self, name: str) -> None:
-        if name is None:
-            raise  # TODO create exception
+    @staticmethod
+    def __validate_name(name: str) -> None:
+        if name is None or name == "":
+            raise ProductIsNoneException("name")
+        if not isinstance(name, str):
+            raise ProductIsInvalidException("name")
 
-    def __validate_price(self, price: float) -> None:
+    @staticmethod
+    def __validate_price(price: float) -> None:
         if price is None:
-            raise  # TODO create exception
+            raise ProductIsNoneException("price")
+        if not isinstance(price, float):
+            raise ProductIsInvalidException("price")
 
-    def get_by_id(self, request: request) -> Product:
-        product_code = request.values["code"]
+    def get_by_id(self, code: str) -> Product:
+        """Find in the database product by id and return a Product object"""
+        product: Optional[Product] = self.database.get_product_by_code(code)
 
-        if product_code is None:
-            # TODO create an exception
-            raise
-
-        product: Product = self.db.get_product_by_code(product_code)
+        if code is None:
+            raise ProductIsNoneException("code")
 
         return product
--- tests/product/test_product_service.py
+++ tests/product/test_product_service.py
@@ -1,5 +1,9 @@
 import unittest
 
+from application.exceptions.product_is_invalid_exception import (
+    ProductIsInvalidException,
+)
+from application.exceptions.product_is_none_exception import ProductIsNoneException
 from application.models.data_base import DataBase
 from application.models.product import Product
 from application.product.product_service import ProductService
@@ -10,21 +14,61 @@ class TestProductService(unittest.TestCase):
         self.db = DataBase(product={}, car=None)
         self.product_service = ProductService(self.db)
 
+        self.db.product["T-SHIRT-B1"] = Product(
+            code="T-SHIRT-B1", name="t-shirt", price=12.12
+        )
+
+    def test_create_when_code_is_none_with_raises_exception(self):
+        request = {"code": None, "name": "t-shirt", "price": 12.12}
+        with self.assertRaises(ProductIsNoneException):
+            self.product_service.create(request)
+
+    def test_create_when_code_is_invalid_with_raises_exception(self):
+        request = {"code": 111, "name": "t-shirt", "price": 12.12}
+        with self.assertRaises(ProductIsInvalidException):
+            self.product_service.create(request)
+
+    def test_create_when_name_is_none_with_raises_exception(self):
+        request = {"code": "T-SHIRT-A1", "name": "", "price": 12.12}
+        with self.assertRaises(ProductIsNoneException):
+            self.product_service.create(request)
+
+    def test_create_when_name_is_invalid_with_raises_exception(self):
+        request = {"code": "T-SHIRT-A1", "name": 111, "price": 12.12}
+        with self.assertRaises(ProductIsInvalidException):
+            self.product_service.create(request)
+
+    def test_create_when_price_is_none_with_raises_exception(self):
+        request = {"code": "T-SHIRT-A1", "name": "t-shirt", "price": None}
+        with self.assertRaises(ProductIsNoneException):
+            self.product_service.create(request)
+
+    def test_create_when_price_is_invalid_with_raises_exception(self):
+        request = {"code": "T-SHIRT-A1", "name": "t-shirt", "price": 12}
+        with self.assertRaises(ProductIsInvalidException):
+            self.product_service.create(request)
+
     def test_create(self):
         request = {"code": "T-SHIRT-A1", "name": "t-shirt", "price": 12.12}
         self.product_service.create(request)
 
         self.assertTrue(self.db.product)
         self.assertCountEqual(
-            {"T-SHIRT-A1": Product(code="T-SHIRT-A1", name="t-shirt", price=12.12)},
+            {
+                "T-SHIRT-B1": Product(code="T-SHIRT-B1", name="t-shirt", price=12.12),
+                "T-SHIRT-A1": Product(code="T-SHIRT-A1", name="t-shirt", price=12.12),
+            },
             self.db.product,
         )
 
-    def test_create_when_code_is_none_with_raises_exception(self):
-        pass
+    def test_get_by_id_when_is_none_exception(self):
+        with self.assertRaises(ProductIsInvalidException):
+            self.product_service.get_by_id("test")
 
-    def test_create_when_name_is_none_with_raises_exception(self):
-        pass
+    def test_get_by_id(self):
+        self.product_service.get_by_id("T-SHIRT-B1")
 
-    def test_create_when_price_is_none_with_raises_exception(self):
-        pass
+        self.assertCountEqual(
+            {"T-SHIRT-B1": Product(code="T-SHIRT-B1", name="t-shirt", price=12.12)},
+            self.db.product,
+        )
