0e4e9f18ac9af0c3b947438270d3697134902720
Daniel Quintana <quintana.danielarmando@gmail.com>
2022-09-22 11:12:48 +0100

    added marshmallow package to serialize data

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,14 @@
 from http import HTTPStatus
 
 from flask import Flask
+from flask_marshmallow import Marshmallow
+
+ma = Marshmallow()
 
 
 def create_app():
     app = Flask(__name__)
+    ma.init_app(app)
 
     with app.app_context():
         from .home import home
